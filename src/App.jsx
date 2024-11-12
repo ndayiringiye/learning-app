4ab7ecd670e1b7c1936af848120cedd008695e8a
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2024-11-12 10:46:10 +0200

    add setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+
+      <h1 className='text-center text-green-600 text-3xl font-bold font-serif'>morning david</h1>
+    </div>
+  )
+}
